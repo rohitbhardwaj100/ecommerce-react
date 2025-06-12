48155e57009f72711bc3e7a7d94e7cc180ee59d4
rohitbhardwaj100 <rohitbhardwaj1179@gmail.com>
2025-06-12 17:18:32 +0530

    🔄 Updated files and features

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ShopContextProvider from './Context/ShopContext.jsx'
+const API = import.meta.env.VITE_API_KEY;
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
